95220d7c3635929b4d2126c36e654d4600855ba8
tminamoto <takao.m@gmail.com>
2016-07-11 23:53:29 -0700

    2nd upload

---
+++ README.md
@@ -0,0 +1,15 @@
+## Project: Neighborhood Map
+
+### Description
+This Map displays drawings of old stations of Todaido, that is the major route in Japan.<br/>
+The paint is called Ukiyo-e.<br/>
+https://en.wikipedia.org/wiki/Ukiyo-e<br/>
+
+### How to run
+Download the entire directory and open 'index.html' in a browser locally.
+
+### Functionality
+Filter to search a station and the result synchronized with a marker in this map.
+Each marker hold information window that display Ukiyo-e of the station.
+Each Ukiyo-e has a link to its wikipedia.
+
--- js/main.js
+++ js/main.js
@@ -1,18 +1,79 @@
 var stations = [
     {
     name: "Nihonbashi",
+    keyword: "Tokaido_Nihonbashi2",
     lat: 35.683611, 
     lng: 139.774444,
+    url: "https://en.wikipedia.org/wiki/Nihonbashi"
     },
     {
     name: "Shinagawa-juku",
+    keyword: "Tokaido01_Shinagawa",
     lat: 35.621944, 
     lng: 139.739167,
+    url: "https://en.wikipedia.org/wiki/Shinagawa-juku"
+    },
+    {
+    name: "Kawasaki-juku",
+    keyword: "Tokaido02_Kawasaki",
+    lat: 35.535556, 
+    lng: 139.707778,
+    url: "https://en.wikipedia.org/wiki/Kawasaki-juku"
+    },
+    {
+    name: "Kanagawa-juku",
+    keyword: "Tokaido03_Kanagawa",
+    lat: 35.472778, 
+    lng: 139.632278,
+    url: "https://en.wikipedia.org/wiki/Kanagawa-juku"
+    },
+    {
+    name: "Hodogaya-juku",
+    keyword: "Tokaido04_Hodogaya",
+    lat: 35.444028, 
+    lng: 139.595556,
+    url: "https://en.wikipedia.org/wiki/Hodogaya-juku"
+    },
+    {
+    name: "Totsuka-juku",
+    keyword: "Tokaido05_Totsuka",
+    lat: 35.395028, 
+    lng: 139.529861,
+    url: "https://en.wikipedia.org/wiki/Totsuka-juku"
+    },
+    {
+    name: "Fujisawa-shuku",
+    keyword: "Tokaido06_Fujisawa",
+    lat: 35.345667, 
+    lng: 139.486306,
+    url: "https://en.wikipedia.org/wiki/Fujisawa-shuku"
+    },
+    {
+    name: "Hiratsuka-juku",
+    keyword: "Tokaido07_Hiratsuka",
+    lat: 35.327278, 
+    lng: 139.337806,
+    url: "https://en.wikipedia.org/wiki/Hiratsuka-juku"
+    },
+    {
+    name: "Ōiso-juku",
+    keyword: "Tokaido08_Oiso",
+    lat: 35.309, 
+    lng: 139.315306,
+    url: "https://en.wikipedia.org/wiki/%C5%8Ciso-juku"
+    },
+    {
+    name: "Odawara-juku",
+    keyword: "Tokaido09_Odawara",
+    lat: 35.248722, 
+    lng: 139.161028,
+    url: "https://en.wikipedia.org/wiki/Odawara-juku"
     }
 ];
 
 
 var infowindow;
+var photourl;
 
 function initialize() {  
 
@@ -21,8 +82,8 @@ function initialize() {
                      });
 
     var mapOptions = {
-        zoom: 9,
-        center: new google.maps.LatLng(35.3606, 138.7278),
+        zoom: 10,
+        center: new google.maps.LatLng(35.472778, 139.632278),
         mapTypeControl: false,
         disableDefaultUI: true
     };
@@ -31,14 +92,25 @@ function initialize() {
 
     setMarkers(stations);
 
+    //Resize Function
+    google.maps.event.addDomListener(window, "resize", function() {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+    });
+    
 }
 
 
 function setMarkers(location) {
 
+    var content;
+
     for(var i=0; i<location.length; i++) {
-        location[i].holdMarker = new google.maps.Marker({
+        var marker = new google.maps.Marker({
           position: new google.maps.LatLng(location[i].lat, location[i].lng),
+          keyword: location[i].keyword,
+          url: location[i].url,
           map: map,
           name: location[i].name,
           animation: google.maps.Animation.DROP, 
@@ -54,31 +126,54 @@ function setMarkers(location) {
           }
         });
 
-        location[i].holdMarker.addListener('click', function() {
-            console.log('click');
+        location[i].holdMarker = marker;
 
-//bounce the clicked maker
-/*
-  if (location[i].holdMarker.getAnimation() !== null) {
-    location[i].holdMarker.setAnimation(null);
-  } else {
-    location[i].holdMarker.setAnimation(google.maps.Animation.BOUNCE);
-  }
-*/
+        marker.addListener('click', function() {
+            //console.log(this);
 
-            // perform ajax request
+            this.setAnimation(google.maps.Animation.BOUNCE);
+            stopAnimation(this);
+
+            getFlickr(this);
+            content = createContent(this);
 
             // open google maps infowindow
-
-  var infowindow = new google.maps.InfoWindow({
-                      content:"Hello World!"
-  });
-
-  infowindow.open(map,location);
+            infowindow.setContent(content);
+            infowindow.open(map,this);
 
         });
     }
+}
 
+// Stop marker bouncing at the 2nd time.
+function stopAnimation(marker) {
+    setTimeout(function () {
+        marker.setAnimation(null);
+    }, 1400);
+}
+
+// Create a content in the info window.
+function createContent(marker) {
+     return marker.name + '<br/>' + '<a href="' + marker.url + '" target="_blank"><img alt="' + marker.name + '" src=' + photourl + '"/></a>';
+}
+
+// Call Flickr API and get Photo URL
+function getFlickr(marker) {
+        var flickr;
+        var photurl;
+        var sourceurl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4fa0d52b3b38c8e04dd7e15efc8e2843&text=' + marker.keyword + '&format=json';
+        $.ajax({
+            url: sourceurl,
+            dataType: 'jsonp',
+            jsonp: 'jsoncallback',
+            success: function(data) {
+                flickr = data.photos.photo;
+                photourl = 'https://farm' + flickr[0].farm + '.staticflickr.com/' + flickr[0].server + '/' + flickr[0].id + '_' + flickr[0].secret + '.jpg';
+            },
+            error: function() {
+                photourl = 'no data';
+            }
+        });
 }
 
 var viewModel = {
@@ -111,4 +206,3 @@ viewModel.stations = ko.dependentObservable(function() {
 
 
 ko.applyBindings(viewModel);
-
